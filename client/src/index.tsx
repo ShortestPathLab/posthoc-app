d57e73788b97845da82e7c76478b1d38f88a29a6
Kevin Zheng <kaiwen530@gmail.com>
2021-08-29 19:55:09 +1000

    Remove unnecessary folder structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "./frontend/js/main";
+import "./js/main";
 
 // ReactDOM.render(
 //   <React.StrictMode>
