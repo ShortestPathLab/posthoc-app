d8b3258cb52771f75293be6f7eff2e3723eba5c1
CamWang <camwang@outlook.com>
2022-12-16 10:57:49 +1100

    add proposal types

---
+++ client/src/components/renderer/planar/types.ts
@@ -0,0 +1,76 @@
+import { Event } from "../types";
+import { Component } from "../types";
+
+type HexColor = `#${string}` | `0x${string}` | number;
+
+// TODO Runtime type check for components and event data
+
+export type Point = {
+  x: number;
+  y: number;
+}
+
+export type BezierPoint = Point & {
+  control1?: Point;
+  control2?: Point;
+}
+
+export type D2IntrinsicComponent = Component & {
+  fill?: HexColor;
+  alpha?: number;
+  text?: string;
+}
+
+export type LineIntrinsicComponent = D2IntrinsicComponent & {
+  $: "line";
+  width: number;
+  point1: Point;
+  point2: Point;
+}
+
+export type RectIntrinsicComponent = D2IntrinsicComponent & {
+  $: "rect";
+  width: number;
+  height: number;
+  point: Point;
+}
+
+export type CircleIntrinsicComponent = D2IntrinsicComponent & {
+  $: "circle";
+  radius: number;
+  point: Point;
+}
+
+export type PolygonIntrinsicComponent = D2IntrinsicComponent & {
+  $: "polygon";
+  points: Point[];
+}
+
+export type BezierPathIntrinsicComponent = D2IntrinsicComponent & {
+  $: "path";
+  path: [BezierPoint, ...BezierPoint[], Point];
+};
+
+/**
+ * Event data types for development reference
+ */
+
+export type LineData = Event & {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+export type RectData = Event & {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export type CircleData = Event & {
+  x: number;
+  y: number;
+  radius: number;
+}
---
+++ client/src/components/renderer/types.ts
@@ -0,0 +1,69 @@
+export type Event = {
+  type: string;
+  id: number;
+  pid?: number;
+  g?: number;
+  h?: number;
+  f?: number;
+  [key: string]: any;
+}
+
+export type Colours = {
+  source: number;
+  destination: number;
+  expanding: number;
+  generating: number;
+  closing: number;
+  end: number;
+}
+
+export type Context = {
+  current: Event | null;
+  parent: Event | null;
+  events: Event[] | null;
+  colour: Colours;
+  [key: string]: any;
+}
+
+export const Default2DRendererContext: Context = {
+  current: null,
+  parent: null,
+  events: null,
+  colour: {
+    source: 0x26a69a,
+    destination: 0xf06292,
+    expanding: 0xff5722,
+    generating: 0xffeb3b,
+    closing: 0xb0bec5,
+    end: 0xec407a,
+  },
+  scale: 15,
+  fill: 0x000000,
+  alpha: 1,
+}
+
+export type Component = {
+  $: string;
+  [key:string]: any;
+}
+
+export type Render = {
+  context?: Context;
+  components?: { 
+    [K: string]: Component 
+  };
+  views: { 
+    main: Component[]; 
+    [K: string]: Component[] 
+  };
+}
+
+export type SearchTrace = {
+  context: object | null;
+  render: Render;
+  eventList: Event[];
+}
+
+export type RenderProps = {
+  searchTrace: SearchTrace;
+}
