d58f11349e4d7871c5fae54f60a595644df4c662
RoryT-U <rorytu@gmail.com>
2023-02-13 10:46:09 +1100

    Added conveters for all the PIXI shape primitives

--- client/src/components/render/renderer/pixi/PixiPrimitives.tsx
+++ client/src/components/render/renderer/pixi/PixiPrimitives.tsx
@@ -39,20 +39,169 @@ export const d2InstrinsicComponents: InstrinsicComponents = {
   "rect": {
     "converter": (comp: Component) => rectDrawingCoverter(comp),
     "renderer": "2d-pixi",
+  },
+  "circle": {
+    "converter": (comp: Component) => circleDrawingCoverter(comp),
+    "renderer": "2d-pixi",
+  },
+  "polygon":{
+    "converter": (comp: Component) => polygonDrawingCoverter(comp),
+    "renderer": "2d-pixi",
+  },
+  "path":{
+    "converter": (comp: Component) => pathDrawingCoverter(comp),
+    "renderer": "2d-pixi",
   }
 }
 
-function rectDrawingCoverter(component: Component) {
+function executeComponent(component: Component, event:Event ){
 
-  return memoizee((event: Event) => {
-    
-    const element:Component = {...component};
+  const element:Component = {...component};
     // Execute the computed props from the event
     for (const prop in component) {
       if (typeof component[prop] === "function") {
         element[prop] = component[prop](event);
       }
     }
+
+  return element
+}
+
+function textElement(text:string|undefined, x:number, y:number){
+    // if text defined, provide text object
+    let textObj: PIXI.Text|undefined = undefined;
+    if (text) {
+      textObj = new PIXI.Text(text, {
+        fontFamily: 'Arial',
+        fontSize: 10,
+        fill: "black",
+      });
+      textObj.y = y
+      textObj.x = x;
+    }
+    return textObj
+
+}
+
+function pathDrawingCoverter(component: Component){
+  return memoizee((event: Event) => {
+    
+    // executes all the computed properties
+    const element:Component = executeComponent(component, event)
+
+    // Determine color by event type
+    let color: number | undefined = undefined;
+    if (event.type !== undefined && event.type in defaultContext.colour) {
+      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
+    }
+    if (!color) {
+      console.error(`No color defined for event type ${event.type} on context`);
+    }
+
+    // calculate shape variables based on result of computed props
+    const points = element.points.map((point:{x:number, y:number}) => {return {x:scale(point.x), y:scale(point.y)}})
+
+    // creates text object (undefined if no text)
+    // TODO text on a polygon (maybe we find the center coordinates based on the points)
+    // const textObj = textElement(element.text, x, y)
+
+    // draw instructions on PIXI graphics
+    return (g: GraphicsType) => {
+      g.beginFill(
+          element.fill ?? color ?? 0xff5722,
+          element.alpha ?? defaultContext.alpha)
+        .drawPolygon(points)
+        .endFill();
+      // if (textObj !== undefined){
+      //   g.addChild(textObj)
+      // }
+    }
+  })
+}
+
+function polygonDrawingCoverter(component: Component){
+  return memoizee((event: Event) => {
+    
+    // executes all the computed properties
+    const element:Component = executeComponent(component, event)
+
+    // Determine color by event type
+    let color: number | undefined = undefined;
+    if (event.type !== undefined && event.type in defaultContext.colour) {
+      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
+    }
+    if (!color) {
+      console.error(`No color defined for event type ${event.type} on context`);
+    }
+
+    // calculate shape variables based on result of computed props
+
+
+    const points = element.points.map((point:{x:number, y:number}) => {return {x:scale(point.x), y:scale(point.y)}})
+
+    // creates text object (undefined if no text)
+    // TODO text on a polygon (maybe we find the center coordinates based on the points)
+    // const textObj = textElement(element.text, x, y)
+
+    // draw instructions on PIXI graphics
+    return (g: GraphicsType) => {
+      g.beginFill(
+          element.fill ?? color ?? 0xff5722,
+          element.alpha ?? defaultContext.alpha)
+        .drawPolygon(points)
+        .endFill();
+      // if (textObj !== undefined){
+      //   g.addChild(textObj)
+      // }
+    }
+  })
+}
+
+function circleDrawingCoverter(component: Component){
+  return memoizee((event: Event) => {
+    
+    // executes all the computed properties
+    const element:Component = executeComponent(component, event)
+
+    // Determine color by event type
+    let color: number | undefined = undefined;
+    if (event.type !== undefined && event.type in defaultContext.colour) {
+      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
+    }
+    if (!color) {
+      console.error(`No color defined for event type ${event.type} on context`);
+    }
+
+    // calculate shape variables based on result of computed props
+    const [x, y, r] = [
+      scale(element.x),
+      scale(element.y),
+      scale(element.radius ?? 1)
+    ];
+
+    // creates text object (undefined if no text)
+    const textObj = textElement(element.text, x, y)
+
+    // draw instructions on PIXI graphics
+    return (g: GraphicsType) => {
+      g.beginFill(
+          element.fill ?? color ?? 0xff5722,
+          element.alpha ?? defaultContext.alpha)
+        .drawCircle(x,y,r)
+        .endFill();
+      if (textObj !== undefined){
+        g.addChild(textObj)
+      }
+    }
+  })
+}
+
+function rectDrawingCoverter(component: Component) {
+
+  return memoizee((event: Event) => {
+    
+    // executes all the computed properties
+    const element:Component = executeComponent(component, event)
 
     // Determine color by event type
     let color: number | undefined = undefined;
@@ -71,17 +220,8 @@ function rectDrawingCoverter(component: Component) {
       scale(element.height ?? 1)
     ];
 
-    // if text defined, provide text object
-    let text: PIXI.Text;
-    if (element.text) {
-      text = new PIXI.Text(element.text, {
-        fontFamily: 'Arial',
-        fontSize: 10,
-        fill: "black",
-      });
-      text.y = y
-      text.x = x;
-    }
+    // creates text object (undefined if no text)
+    const textObj = textElement(element.text, x, y)
 
     // draw instructions on PIXI graphics
     return (g: GraphicsType) => {
@@ -90,8 +230,8 @@ function rectDrawingCoverter(component: Component) {
           element.alpha ?? defaultContext.alpha)
         .drawRect(x, y, w, h)
         .endFill();
-      if (text !== undefined){
-        g.addChild(text)
+      if (textObj !== undefined){
+        g.addChild(textObj)
       }
     }
   })
