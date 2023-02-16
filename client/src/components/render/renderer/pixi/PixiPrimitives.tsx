7acfabdc9f785db5ea277ed967bf1034c34e7a62
Rory T-U <rorytu@gmail.com>
2023-02-16 17:18:35 +1100

    Improved the PixiPrimitive code Added some todos

--- client/src/components/render/renderer/pixi/PixiPrimitives.tsx
+++ client/src/components/render/renderer/pixi/PixiPrimitives.tsx
@@ -3,7 +3,7 @@ import { Component, Event } from "../../types/render";
 import * as PIXI from 'pixi.js';
 import memoizee from "memoizee";
 
-export type DrawInstruction = ((event: Event) => (graphic: GraphicsType) => void) & {persisted?: boolean};
+export type DrawInstruction = ((eventContext: EventContext) => (graphic: GraphicsType) => void) & { persisted?: boolean };
 
 export type InstrinsicComponents = {
   [key: string]: {
@@ -11,6 +11,15 @@ export type InstrinsicComponents = {
     "renderer": string
   }
 }
+
+export type EventContext = {
+  parent: Event | undefined,
+  allEvents: Event[],
+  colour: {
+    [key: string]: number
+  }
+  scale?: number
+} & Event
 
 // default context
 const defaultContext = {
@@ -37,206 +46,119 @@ const scale = (length: number): number => {
 
 export const d2InstrinsicComponents: InstrinsicComponents = {
   "rect": {
-    "converter": (comp: Component) => rectDrawingCoverter(comp),
+    "converter": (comp: Component) => pixiInterlangConventer(comp),
     "renderer": "2d-pixi",
   },
   "circle": {
-    "converter": (comp: Component) => circleDrawingCoverter(comp),
+    "converter": (comp: Component) => pixiInterlangConventer(comp),
     "renderer": "2d-pixi",
   },
-  "polygon":{
-    "converter": (comp: Component) => polygonDrawingCoverter(comp),
+  "polygon": {
+    "converter": (comp: Component) => pixiInterlangConventer(comp),
     "renderer": "2d-pixi",
   },
-  "path":{
-    "converter": (comp: Component) => pathDrawingCoverter(comp),
+  "path": {
+    "converter": (comp: Component) => pixiInterlangConventer(comp),
     "renderer": "2d-pixi",
   }
 }
 
-function executeComponent(component: Component, event:Event ){
+function executeComponent(component: Component, event: Event) {
 
-  const element:Component = {...component};
-    // Execute the computed props from the event
-    for (const prop in component) {
-      if (typeof component[prop] === "function") {
-        element[prop] = component[prop](event);
-      }
+  const element: Component = { ...component };
+  // Execute the computed props from the event
+  for (const prop in component) {
+    if (typeof component[prop] === "function") {
+      element[prop] = component[prop](event);
     }
+  }
 
   return element
 }
 
-function textElement(text:string|undefined, x:number, y:number){
-    // if text defined, provide text object
-    let textObj: PIXI.Text|undefined = undefined;
-    if (text) {
-      textObj = new PIXI.Text(text, {
-        fontFamily: 'Arial',
-        fontSize: 1,
-        fill: "black",
-      });
-      textObj.y = y
-      textObj.x = x;
-      textObj.resolution = 100
-    }
-    return textObj
+function textElement(text: string | undefined, x: number, y: number) {
+  // if text defined, provide text object
+  let textObj: PIXI.Text | undefined = undefined;
+  if (text) {
+    textObj = new PIXI.Text(text, {
+      fontFamily: 'Arial',
+      fontSize: 1,
+      fill: "black",
+    });
+    textObj.y = y
+    textObj.x = x;
+    textObj.resolution = 100
+  }
+  return textObj
 
 }
 
-function pathDrawingCoverter(component: Component){
-  return memoizee((event: Event) => {
-    
-    // executes all the computed properties
-    const element:Component = executeComponent(component, event)
-
-    // Determine color by event type
-    let color: number | undefined = undefined;
-    if (event.type !== undefined && event.type in defaultContext.colour) {
-      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
-    }
-    if (!color) {
-      console.error(`No color defined for event type ${event.type} on context`);
-    }
-
-    // calculate shape variables based on result of computed props
-    const points = element.points.map((point:{x:number, y:number}) => {return {x:scale(point.x), y:scale(point.y)}})
-
-    // creates text object (undefined if no text)
-    // TODO text on a polygon (maybe we find the center coordinates based on the points)
-    // const textObj = textElement(element.text, x, y)
-
-    // draw instructions on PIXI graphics
-    return (g: GraphicsType) => {
-      g.beginFill(
-          element.fill ?? color ?? 0xff5722,
-          element.alpha ?? defaultContext.alpha)
-        .drawPolygon(points)
-        .endFill();
-      // if (textObj !== undefined){
-      //   g.addChild(textObj)
-      // }
-    }
-  })
-}
-
-function polygonDrawingCoverter(component: Component){
-  return memoizee((event: Event) => {
-    
-    // executes all the computed properties
-    const element:Component = executeComponent(component, event)
-
-    // Determine color by event type
-    let color: number | undefined = undefined;
-    if (event.type !== undefined && event.type in defaultContext.colour) {
-      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
-    }
-    if (!color) {
-      console.error(`No color defined for event type ${event.type} on context`);
-    }
-
-    // calculate shape variables based on result of computed props
-
-
-    const points = element.points.map((point:{x:number, y:number}) => {return {x:scale(point.x), y:scale(point.y)}})
-
-    // creates text object (undefined if no text)
-    // TODO text on a polygon (maybe we find the center coordinates based on the points)
-    // const textObj = textElement(element.text, x, y)
-
-    // draw instructions on PIXI graphics
-    return (g: GraphicsType) => {
-      g.beginFill(
-          element.fill ?? color ?? 0xff5722,
-          element.alpha ?? defaultContext.alpha)
-        .drawPolygon(points)
-        .endFill();
-      // if (textObj !== undefined){
-      //   g.addChild(textObj)
-      // }
-    }
-  })
-}
-
-function circleDrawingCoverter(component: Component){
-
-  return memoizee((event: Event) => {
-    
-    // executes all the computed properties
-    const element:Component = executeComponent(component, event)
-
-    // Determine color by event type
-    let color: number | undefined = undefined;
-    if (event.type !== undefined && event.type in defaultContext.colour) {
-      color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
-    }
-    if (!color) {
-      console.error(`No color defined for event type ${event.type} on context`);
-    }
-
-    // calculate shape variables based on result of computed props
-    const [x, y, r] = [
-      scale(element.x),
-      scale(element.y),
-      scale(element.radius ?? 1)
-    ];
-
-    // creates text object (undefined if no text)
-    const textObj = textElement(element.text, x, y)
-
-    // draw instructions on PIXI graphics
-    return (g: GraphicsType) => {
-      g.beginFill(
-          element.fill ?? color ?? 0xff5722,
-          element.alpha ?? defaultContext.alpha)
-        .drawCircle(x,y,r)
-        .endFill();
-      if (textObj !== undefined){
-        g.addChild(textObj)
-      }
-    }
-  })
-}
-
-function rectDrawingCoverter(component: Component) {
-  function drawInstruction(event: Event) {
-    if (event) {
-        // executes all the computed properties
-      const element:Component = executeComponent(component, event)
+function pixiInterlangConventer(component: Component) {
+  function drawInstruction(eventContext: EventContext) {
+    if (eventContext) {
+      // executes all the computed properties
+      const element: Component = executeComponent(component, eventContext)
 
       // Determine color by event type
       let color: number | undefined = undefined;
-      if (event.type !== undefined && event.type in defaultContext.colour) {
-        color = defaultContext.colour[event.type as keyof typeof defaultContext.colour];
+      if (eventContext.type !== undefined && eventContext.type in defaultContext.colour) {
+        color = defaultContext.colour[eventContext.type as keyof typeof defaultContext.colour];
       }
       if (!color) {
-        console.error(`No color defined for event type ${event.type} on context`);
+        console.error(`No color defined for event type ${eventContext.type} on context`);
       }
 
-
-      // calculate shape variables based on result of computed props
-      const [x, y, w, h] = [
-        scale(element.x),
-        scale(element.y),
-        scale(element.width ?? 1),
-        scale(element.height ?? 1)
-      ];
-
-      const textObj = textElement(element.text, x+w/6, y-h/4)
-
+      let textObj: PIXI.Text | undefined = undefined;;
       // draw instructions on PIXI graphics
       return (g: GraphicsType) => {
-        g.beginFill(
-            element.fill ?? color ?? 0xff5722,
+
+
+        g.beginFill(element.fill ?? color ?? 0xff5722,
             element.alpha ?? defaultContext.alpha)
-          .drawRect(x, y, w, h)
-          .endFill();
-        if (textObj !== undefined){
+
+        switch (component.$) {
+          case "rect":
+            let [rectX, rectY, rectW, rectH] = [
+              scale(element.x),
+              scale(element.y),
+              scale(element.width ?? 1),
+              scale(element.height ?? 1)
+            ];
+
+
+            textObj = textElement(element.text, rectX+rectW/6, rectY-rectH/4)
+            g.drawRect(rectX, rectY, rectW, rectH)
+            break;
+
+          case "circle":
+            let [circX, circY, circR] = [
+              scale(element.x),
+              scale(element.y),
+              scale(element.radius ?? 1)
+            ];
+            g.drawCircle(circX, circY, circR)
+            break;
+          case "path":
+            // fallthrough is wanted, as polygons and paths are drawn the same way
+            g.lineStyle({ width: 0.2 })
+          case "polygon":
+            const points = element.points.map((point: { x: number, y: number }) => { return { x: scale(point.x), y: scale(point.y) } })
+
+            g.drawPolygon(points)
+            break;
+
+          default:
+            console.log(element.$)
+            throw Error("Invalid primitive for the PIXI renderer")
+        }
+
+        g.endFill()
+        if (textObj !== undefined) {
           g.addChild(textObj)
         }
       }
     } else {
-      return (g: GraphicsType) => {}
+      return (g: GraphicsType) => { }
     }
   }
 
@@ -244,3 +166,5 @@ function rectDrawingCoverter(component: Component) {
 
   return drawInstruction;
 }
+
+
--- client/src/components/render/renderer/pixi/PixiStage.tsx
+++ client/src/components/render/renderer/pixi/PixiStage.tsx
@@ -4,7 +4,7 @@ import { Stage } from "@inlet/react-pixi";
 
 import { Event, View } from "components/render/types/render";
 import { Viewport } from "./Viewport";
-import { d2InstrinsicComponents, DrawInstruction} from "./PixiPrimitives"
+import { d2InstrinsicComponents, DrawInstruction } from "./PixiPrimitives"
 import { StageChild } from '../types';
 import { PixiViewport } from './PixiViewport';
 
@@ -19,7 +19,17 @@ export type PixiStageProps = {
 }
 
 export type DrawInstructions = {
-  [key:string]:DrawInstruction
+  [key: string]: DrawInstruction
+}
+
+function findRecentParentEvent(pId:number|string|undefined|null, allEvents:Event[]):Event|undefined{
+
+  for (let i = allEvents.length - 1; i >= 0; i--){
+    if (allEvents[i]?.id === pId){
+      return allEvents[i]
+    }
+  }
+  return undefined
 }
 
 /**
@@ -42,13 +52,13 @@ export function PixiStage(
   const viewport = React.useRef<PixiViewport>(null);
 
   // process all the parsed components into drawing instructions 
-  const drawInstructs:DrawInstructions = useMemo(() => {
+  const drawInstructs: DrawInstructions = useMemo(() => {
     if (!view) {
       throw new Error("No view is present in PixiStageProps");
     }
     const viewComps = view.components;
-    const drawInstructions:DrawInstructions = {};
-    for (const compName in viewComps){
+    const drawInstructions: DrawInstructions = {};
+    for (const compName in viewComps) {
       const component = viewComps[compName as keyof object]
       drawInstructions[compName] = d2InstrinsicComponents[component.$].converter(component);
     }
@@ -56,22 +66,58 @@ export function PixiStage(
   }, [view])
 
   // a function which takes in an Event List creates a graphic for them
-  const makeGraphic = React.useCallback((events:Event[], hasCurrent:boolean)=>{
+  const makeGraphic = React.useCallback((events: Event[], hasCurrent: boolean) => {
     // loops through all the events and the drawing instructions
     // adding them all to the PIXI graphic
+
+    const eventContext = {
+      allEvents:events,
+      colour: {
+        source: 0x26a69a,
+        destination: 0xf06292,
+        expanding: 0xff5722,
+        updating: 0xff5722,
+        generating: 0xffeb3b,
+        closing: 0xb0bec5,
+        end: 0xec407a,
+      },
+    }
     const g = new PIXI.Graphics();
-    for (const compName in drawInstructs){
+    for (const compName in drawInstructs) {
       const drawInstruction = drawInstructs[compName];
 
+
+
       if (drawInstruction.persisted === true) {
-        for (const event of events){
+        for (const event of events) {
           // create the context here
           // spread the current event and get the parent event aswell
 
-          drawInstruction(event)(g);
+          let parentEvent = findRecentParentEvent(event?.pId, events)
+          
+          // TODO fix this parent section 
+          if (parentEvent === undefined){
+            parentEvent = event
+          }
+
+          // TODO fix how the currentEventContext is created
+          const currentEventContext = { ...eventContext, parent:parentEvent, ...event}
+
+
+          drawInstruction(currentEventContext)(g);
         }
-      } else if (events[events.length - 1 ] && hasCurrent) {
-        drawInstruction(events[events.length - 1 ])(g);
+      } else if (events[events.length - 1] && hasCurrent) {
+        const curEvent = events[events.length - 1]
+          // TODO fix this parent section 
+        let parentEvent = findRecentParentEvent(curEvent?.pId, events)
+
+        if (parentEvent === undefined){
+          parentEvent = curEvent
+        }
+
+
+        const currentEventContext = { ...eventContext, parent:parentEvent, ...curEvent}
+        drawInstruction(currentEventContext)(g);
       }
     }
     return g;
@@ -79,8 +125,8 @@ export function PixiStage(
 
   // create an add function that adds the graphic to a canvas and then returns a remove function
   const canvas = React.useCallback(
-    ()=>({
-      add:(events:Event[], hasCurrent:boolean)=>{
+    () => ({
+      add: (events: Event[], hasCurrent: boolean) => {
         const graphic = makeGraphic(events, hasCurrent);
         viewport.current?.addChild?.(graphic);
         return () => {
@@ -89,7 +135,7 @@ export function PixiStage(
       }
     }), [makeGraphic]
   )
-  
+
   return (<>
     <Stage
       width={width} height={height}
@@ -101,19 +147,19 @@ export function PixiStage(
         antialias: true,
       }}
     >
-      <Viewport ref={viewport} width={width} height={height}/>
+      <Viewport ref={viewport} width={width} height={height} />
       {
-          /**
-           * Children will be a callback that returns child components 
-           * wrapped in Fragment and binded with useCanvas prop
-           * (useCanvas) => (
-           *  <React.Fragment>
-           *    <LazyNodeList useCanvas={useCanvas} />
-           *  </React.Fragment>
-           * )
-           */
-          children?.(canvas)
-        }
+        /**
+         * Children will be a callback that returns child components 
+         * wrapped in Fragment and binded with useCanvas prop
+         * (useCanvas) => (
+         *  <React.Fragment>
+         *    <LazyNodeList useCanvas={useCanvas} />
+         *  </React.Fragment>
+         * )
+         */
+        children?.(canvas)
+      }
     </Stage>
   </>)
 }
