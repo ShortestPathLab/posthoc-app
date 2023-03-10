9394bea97acfe39e1fb63627f413cb7a06ec0017
CamWang <camwang@outlook.com>
2023-03-10 16:46:54 +1100

    fix welcome hack error

--- client/src/components/app-bar/Title.tsx
+++ client/src/components/app-bar/Title.tsx
@@ -1,43 +1,46 @@
-import { Typography as Type, BoxProps, Box } from "@material-ui/core";
-import {
-  AppBarTitle,
-  ManagedModal as Dialog,
-} from "components/generic/Modal";
+import { Typography as Type, BoxProps, Box, DialogTitle, IconButton, DialogContent, Dialog } from "@material-ui/core";
 import { Welcome } from "components/welcome/Welcome"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { bindTrigger, bindPopover, usePopupState } from "material-ui-popup-state/hooks";
+import CloseIcon from '@material-ui/icons/Close';
 
 export function Title(props: BoxProps) {
-  const [first, setFirst] = useState(true);
+  const [open, setOpen] = useState(true);
 
   return (
-    <>
-      <Dialog
-        trigger={(onClick, setOpen) => {
-          if (first) {
-            setOpen?.(true);
-            setFirst(false);
-          }
-          return (
-            <Box sx={{cursor:"pointer", userSelect:"none"}} bgcolor="primary.main" p={2} {...{onClick}}>
-              <Type
-                variant="body1"
-                color="text.secondary"
-                sx={{
-                  color: "primary.contrastText",
-                  whiteSpace: "nowrap",
-                  fontWeight: 500,
-                }}
-              >
-                PFAlgoViz
-              </Type>
-            </Box>
-          )
-        }}
-        options={{width:960, scrollable: false}}
-        appBar={{children: <AppBarTitle>Welcome</AppBarTitle> }}
-      >
-        <Welcome />
+    <div>
+      <Box sx={{cursor:"pointer", userSelect:"none"}} bgcolor="primary.main" p={2} onClick={() => setOpen(!open)}>
+        <Type
+          variant="body1"
+          color="text.secondary"
+          sx={{
+            color: "primary.contrastText",
+            whiteSpace: "nowrap",
+            fontWeight: 500,
+          }}
+        >
+          PFAlgoViz
+        </Type>
+      </Box>
+      <Dialog maxWidth={'md'} fullWidth={true} open={open} onClose={() => setOpen(false)}>
+        <DialogTitle sx={{ m: 0, p: 2 }}>
+          <IconButton
+              aria-label="close"
+              onClick={() => setOpen(false)}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <Welcome />
+        </DialogContent>
       </Dialog>
-    </>
+    </div>
   );
 }
