76a062ff117ee86f860c817026a543cb3e9385fd
Kevin Zheng <kaiwen530@gmail.com>
2023-11-16 11:31:42 +1100

    Fix issue with parser

--- client/src/components/renderer/parser/parseTrace.worker.ts
+++ client/src/components/renderer/parser/parseTrace.worker.ts
@@ -1,4 +1,4 @@
-import { chunk, flatMap, map, range } from "lodash";
+import { ceil, flatMap, flatten, map, range } from "lodash";
 import { usingWorkerTask } from "../../../workers/usingWorker";
 import {
   ParseTraceWorkerParameters,
@@ -9,7 +9,6 @@ import parseTraceWorkerUrl from "./parseTraceSlave.worker.ts?worker&url";
 const { min } = Math;
 
 const SLAVE_COUNT = navigator.hardwareConcurrency ?? 8;
-const CHUNK_SIZE = 16;
 
 export class ParseTraceWorker extends Worker {
   constructor() {
@@ -27,25 +26,21 @@ async function parse({
   context,
   view = "main",
 }: ParseTraceWorkerParameters): Promise<ParseTraceWorkerReturnType> {
-  const chunks = range(0, trace?.events?.length, CHUNK_SIZE);
-  const tasks = chunk(chunks, SLAVE_COUNT);
-  const outs = [];
-
-  for (const task of tasks) {
-    outs.push(
-      ...(await Promise.all(
-        map(task, (i) =>
-          parseTraceWorker({
-            trace,
-            context,
-            view,
-            from: i,
-            to: min(i + CHUNK_SIZE, trace?.events?.length ?? 0),
-          })
-        )
-      ))
-    );
-  }
+  const chunkSize = ceil((trace?.events?.length ?? 0) / SLAVE_COUNT);
+  const chunks = range(0, trace?.events?.length, chunkSize);
+  const outs = flatten(
+    await Promise.all(
+      map(chunks, (i) =>
+        parseTraceWorker({
+          trace,
+          context,
+          view,
+          from: i,
+          to: min(i + chunkSize, trace?.events?.length ?? 0),
+        })
+      )
+    )
+  );
 
   return {
     stepsPersistent: flatMap(outs, "stepsPersistent"),
