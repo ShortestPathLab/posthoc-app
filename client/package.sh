62b904362ac0eb25faa675c9f9a485d95cdcf714
Kevin Zheng <kaiwen530@gmail.com>
2024-02-16 01:54:48 +1100

    Add release workflow

--- client/package.sh
+++ client/package.sh
@@ -1,2 +1,7 @@
 npx electron-packager . Visualiser --dir dist --platform=win32 --arch=x64 --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
 npx electron-packager . Visualiser --dir dist --platform=linux --arch=x64 --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
+
+cd bin
+
+zip -r Visualiser-linux-x64.zip Visualiser-linux-x64
+zip -r Visualiser-win32-x64.zip Visualiser-win32-x64
