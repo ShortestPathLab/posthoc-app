5df0fb3953cce1d7a35b59dbc24b95520fb09956
Kevin Zheng <kaiwen530@gmail.com>
2024-05-26 18:09:35 +1000

    Update build for mac

--- client/package.sh
+++ client/package.sh
@@ -1,9 +1,9 @@
 npx electron-packager . Posthoc --dir dist --platform=win32 --arch=x64 --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
 npx electron-packager . Posthoc --dir dist --platform=linux --arch=x64 --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
-# npx electron-packager . Posthoc --dir dist --platform=darwin --arch=universal --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
+npx electron-packager . Posthoc --dir dist --platform=darwin --arch=arm64 --electronVersion=26.2.1 --ignore node_modules --ignore src --overwrite --icon ./dist/favicon --out bin
 
 cd bin
 
 zip -r Posthoc-linux-x64.zip Posthoc-linux-x64
 zip -r Posthoc-win32-x64.zip Posthoc-win32-x64
-# zip -r Posthoc-darwin-universal.zip Posthoc-darwin-universal
+zip -r Posthoc-darwin-arm64.zip Posthoc-darwin-arm64
