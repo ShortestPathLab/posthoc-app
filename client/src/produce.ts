30af96472023a890907589fb94ac4253db609ba4
Kevin Zheng <kaiwen530@gmail.com>
2025-01-24 14:06:12 +1100

    Add async versions of produce functions

--- client/src/produce.ts
+++ client/src/produce.ts
@@ -1,15 +1,28 @@
 import { EditorSetterProps } from "components/Editor";
 import { clone } from "lodash";
-import { ReactNode, createElement } from "react";
+import { createElement, ReactElement } from "react";
 
 export function produce<T>(obj: T, f: (obj: T) => void) {
   const b = clone(obj);
   f(b);
   return b;
 }
-export function transaction<T, U>(obj: T, f: (obj: T) => U) {
-  const b = f(clone(obj));
+
+export async function produceAsync<T>(obj: T, f: (obj: T) => Promise<void>) {
+  const b = clone(obj);
+  await f(b);
   return b;
+}
+
+export function transaction<T, U>(obj: T, f: (obj: T) => U) {
+  return f(clone(obj));
+}
+
+export async function transactionAsync<T, U>(
+  obj: T,
+  f: (obj: T) => Promise<U>
+) {
+  return await f(clone(obj));
 }
 
 export const producify =
@@ -25,7 +38,7 @@ export function withProduce<T>(
     props: EditorSetterProps<T> & {
       produce: (f: (obj: T) => void) => void;
     }
-  ) => ReactNode
+  ) => ReactElement
 ) {
   return (props: EditorSetterProps<T>) =>
     createElement(component, {
