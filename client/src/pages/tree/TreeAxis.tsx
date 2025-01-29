eb4913e8a55f8012b09c8a867762629bb1f02d26
Yihan Huang <huangehan@gmail.com>
2025-01-29 15:45:44 +1100

    feat: g value

--- client/src/pages/tree/TreeAxis.tsx
+++ client/src/pages/tree/TreeAxis.tsx
@@ -2,38 +2,55 @@ import { useEffect, useRef, useState } from "react";
 import { TreeWorkerReturnType } from "./tree.worker";
 import { Trace } from "protocol";
 import { CameraState, Coordinates } from "sigma/types";
-import { chain, isUndefined, List, map } from "lodash";
+import { chain, filter, forOwn, isUndefined, List, map } from "lodash";
 import { sort } from "moderndash";
 import { useRegisterEvents, useSigma } from "@react-sigma/core";
+import { useThrottle } from "react-use";
 
 type TreeAxisProps = {
   tree?: TreeWorkerReturnType;
   trace?: Trace;
   width?: number;
   height?: number;
+  axisTrackingValue?: string;
+};
+
+type node = {
+  x?: number;
+  y?: number;
+  id?: string | number;
+  step?: number;
+  f?: number;
+  g?: number;
+  [key: string]: any;
 };
 
 export function TreeAxis(props: TreeAxisProps) {
+  const {
+    width = 0,
+    height = 0,
+    trace,
+    tree: orignalTree,
+    axisTrackingValue,
+  } = props;
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const { width = 0, height = 0, trace } = props;
   const events = trace?.events;
-  const sigma = useSigma();
-  const registerEvents = useRegisterEvents();
 
-  const graph = sigma.getGraph();
-  const [tree, setTree] = useState<List<Coordinates>>([{ x: 0, y: 0 }]);
+  const registerEvents = useRegisterEvents();
+  const [tree, setTree] = useState<List<node>>();
+  const sigma = useSigma();
+  const nodesData = chain(events)
+    .map((c, i) => ({ step: i, id: c.id, pId: c.pId, g: c.g }))
+    .groupBy("id")
+    .value();
 
   useEffect(() => {
     function updateTree() {
-      const nodes = graph.nodes();
-      const updatedTree = map(nodes, (node: string) => {
-        const displayData = sigma.getNodeDisplayData(node);
-        const y = displayData?.y;
-        let coordinates = { x: 0, y: 0 };
-        if (!isUndefined(y)) {
-          coordinates = sigma.framedGraphToViewport({ x: 0, y });
-        }
-        return coordinates;
+      const updatedTree = map(orignalTree, ({ x, y, ...node }) => {
+        return {
+          ...nodesData[node.label][0],
+          ...sigma.graphToViewport({ x, y }),
+        };
       });
       setTree(updatedTree);
     }
@@ -57,18 +74,39 @@ export function TreeAxis(props: TreeAxisProps) {
 
     const padding = 5;
 
-    const yAxis = chain(tree).groupBy("y").value();
-    console.log(tree);
-    // const nodesData = chain(events)
-    //   .map((c, i) => ({ step: i, id: c.id, pId: c.pId, g: c.g }))
-    //   .groupBy("id")
-    //   .value();
+    // check the ratio of camera
+    const camera = sigma.getCamera();
+    const { ratio } = camera.getState();
+    const isSkip = Math.round(ratio);
 
-    const yAxisData = sort(
+    const yAxis = chain(tree).groupBy("y").value();
+
+    const yAxisArray = sort(
       Object.keys(yAxis).map((e) => Number(e)),
       { order: "asc" }
     );
+    let yAxisData = [];
+    if (isSkip >= 1) {
+      for (let i = 0; i < yAxisArray.length; ) {
+        yAxisData.push(yAxisArray[i]);
+        i += isSkip + 1;
+      }
+    } else {
+      yAxisData = yAxisArray;
+    }
 
+    // g value
+    const labelText: { [key: string]: [string, string] } = {};
+
+    forOwn(yAxis, (value, key) => {
+      const ave =
+        value.reduce((acc, curr) => acc + (curr?.g ?? 0), 0) / value.length;
+      const [coefficient, exp] = ave
+        .toExponential(2)
+        .split("e")
+        .map((item) => +item);
+      labelText[key] = [coefficient.toString(), exp.toString()];
+    });
     const maxY = Math.max(...yAxisData);
     const minY = Math.min(...yAxisData);
 
@@ -79,6 +117,11 @@ export function TreeAxis(props: TreeAxisProps) {
     ctx.lineWidth = 1;
     ctx.stroke();
 
+    ctx.font = "10px Arial";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Y Axis: G Value", padding, height * 0.98);
+
     for (let i = 0; i <= yAxisData.length; i++) {
       ctx.beginPath();
       ctx.moveTo(padding, yAxisData[i]);
@@ -86,9 +129,18 @@ export function TreeAxis(props: TreeAxisProps) {
       ctx.stroke();
 
       ctx.font = "10px Arial";
-      ctx.textAlign = "right";
+      ctx.textAlign = "left";
       ctx.textBaseline = "middle";
-      ctx.fillText(yAxisData[i]?.toFixed(2), padding + 38, yAxisData[i]);
+      ctx.fillText(labelText[yAxisData[i]]?.[0], padding + 5, yAxisData[i]);
+
+      ctx.font = "8px Arial";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "middle";
+      ctx.fillText(
+        labelText[yAxisData[i]]?.[1],
+        padding + 25,
+        yAxisData[i] - 4
+      );
     }
   }, [sigma, tree, width, height]);
 
--- client/src/pages/tree/TreePage.tsx
+++ client/src/pages/tree/TreePage.tsx
@@ -4,6 +4,7 @@ import {
   LayersOutlined as LayersIcon,
   ModeStandbyOutlined,
   TimelineOutlined,
+  LineAxisOutlined,
 } from "@mui-symbols-material/w400";
 import {
   Box,
@@ -108,6 +109,8 @@ export function TreePage({ template: Page }: PageContentProps) {
   const [trackedProperty, setTrackedProperty, properties] =
     useTrackedProperty(trace);
 
+  // const [axisTracking, setAxisTracking] = useState<typeof properties | "">("");
+
   const { point, selected, selection, setSelection } = useSelection(
     step,
     trace
@@ -194,13 +197,15 @@ export function TreePage({ template: Page }: PageContentProps) {
                               setMenuOpen(true);
                             }}
                           />
-                          <TreeAxis
-                            tree={tree}
-                            trace={trace}
-                            key={key}
-                            height={size.height}
-                            width={size.width}
-                          ></TreeAxis>
+                          {trackedProperty && (
+                            <TreeAxis
+                              tree={tree}
+                              trace={trace}
+                              key={key}
+                              height={size.height}
+                              width={size.width}
+                            ></TreeAxis>
+                          )}
                         </SigmaContainer>
                       </>
                     )}
@@ -437,6 +442,16 @@ export function TreePage({ template: Page }: PageContentProps) {
                 ...map(properties, (k) => ({ id: k, name: `$.${k}` })),
               ],
             },
+            // {
+            //   icon: <LineAxisOutlined />,
+            //   label: "Axis Tracking",
+            //   value: axisTracking,
+            //   onChange: setAxisTracking,
+            //   items: [
+            //     { id: "", name: "Off" },
+            //     ...map(properties, (k) => ({ id: k, name: `$.${k}` })),
+            //   ],
+            // },
           ],
           ({ icon, label, value, items, onChange }, i) => (
             <>
