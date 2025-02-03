40c6188d6e74c420fed52e36e49600311dae6a63
Kevin Zheng <kaiwen530@gmail.com>
2025-02-04 04:02:25 +1100

    Add assert utility

---
+++ client/src/utils/assert.ts
@@ -0,0 +1,22 @@
+export class AssertionError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "AssertionError";
+  }
+}
+
+/**
+ * Asserts a condition. If the condition is false, throws the given error.
+ * If the message is a string, it is used to construct an AssertionError.
+ *
+ * @param condition the condition to assert
+ * @param message the error to throw if the condition is false
+ */
+export function assert(
+  condition: unknown,
+  message: string | Error
+): asserts condition {
+  if (!condition) {
+    throw message instanceof Error ? message : new AssertionError(message);
+  }
+}
