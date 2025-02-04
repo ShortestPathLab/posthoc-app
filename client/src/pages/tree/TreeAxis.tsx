f5e2a8304604cccd7bfbec45520eff5c3c5ab3cd
Kevin Zheng <kaiwen530@gmail.com>
2025-02-05 01:10:12 +1100

    Minor refactors for TreeAxis

--- client/src/pages/tree/TreeAxis.tsx
+++ client/src/pages/tree/TreeAxis.tsx
@@ -1,8 +1,8 @@
 import { useRegisterEvents, useSigma } from "@react-sigma/core";
-import { chain, forOwn, List, map } from "lodash";
+import { chain, forOwn, map } from "lodash";
 import { sort } from "moderndash";
 import { Trace } from "protocol";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { TreeWorkerReturnType } from "./tree.worker";
 
 type TreeAxisProps = {
@@ -13,7 +13,7 @@ type TreeAxisProps = {
   axisTrackingValue?: string;
 };
 
-type node = {
+type Node = {
   x?: number;
   y?: number;
   id?: string | number;
@@ -36,13 +36,98 @@ export function TreeAxis(props: TreeAxisProps) {
   const sigma = useSigma();
   const registerEvents = useRegisterEvents();
 
-  const [tree, setTree] = useState<List<node>>();
-  const nodesData = chain(events)
-    .map((c, i) => ({ step: i, id: c.id, pId: c.pId, g: c.g }))
-    .groupBy("id")
-    .value();
+  const [tree, setTree] = useState<Node[]>();
+
+  const drawAxis = useCallback(
+    (tree: Node[]) => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.clearRect(0, 0, width!, height!);
+
+      const padding = 5;
+
+      // check the ratio of camera
+      const camera = sigma.getCamera();
+      const { ratio } = camera.getState();
+      const isSkip = Math.round(ratio);
+
+      const yAxis = chain(tree).groupBy("y").value();
+
+      const yAxisArray = sort(
+        Object.keys(yAxis).map((e) => Number(e)),
+        { order: "asc" }
+      );
+      let yAxisData = [];
+      if (isSkip >= 1) {
+        for (let i = 0; i < yAxisArray.length; ) {
+          yAxisData.push(yAxisArray[i]);
+          i += isSkip + 1;
+        }
+      } else {
+        yAxisData = yAxisArray;
+      }
+
+      // g value
+      const labelText: { [key: string]: [string, string] } = {};
+
+      forOwn(yAxis, (value, key) => {
+        const ave =
+          value.reduce((acc, curr) => acc + (curr?.g ?? 0), 0) / value.length;
+        const [coefficient, exp] = ave
+          .toExponential(2)
+          .split("e")
+          .map((item) => +item);
+        labelText[key] = [coefficient.toString(), exp.toString()];
+      });
+      const maxY = Math.max(...yAxisData);
+      const minY = Math.min(...yAxisData);
+
+      ctx.beginPath();
+      ctx.moveTo(5, minY);
+      ctx.lineTo(5, maxY);
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 1;
+      ctx.stroke();
+
+      ctx.font = "10px Arial";
+      ctx.textAlign = "left";
+      ctx.textBaseline = "top";
+      ctx.fillText("Y Axis: G Value", padding, height * 0.98);
+
+      for (let i = 0; i <= yAxisData.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(padding, yAxisData[i]);
+        ctx.lineTo(padding + 5, yAxisData[i]);
+        ctx.stroke();
+
+        ctx.font = "10px Arial";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "middle";
+        ctx.fillText(labelText[yAxisData[i]]?.[0], padding + 5, yAxisData[i]);
+
+        ctx.font = "8px Arial";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "middle";
+        ctx.fillText(
+          labelText[yAxisData[i]]?.[1],
+          padding + 25,
+          yAxisData[i] - 4
+        );
+      }
+    },
+    [sigma, width, height]
+  );
 
   useEffect(() => {
+    const nodesData = chain(events)
+      .map((c, i) => ({ step: i, id: c.id, pId: c.pId, g: c.g }))
+      .groupBy("id")
+      .value();
+
     function updateTree() {
       const updatedTree = map(orignalTree, ({ x, y, ...node }) => {
         return {
@@ -50,7 +135,7 @@ export function TreeAxis(props: TreeAxisProps) {
           ...sigma.graphToViewport({ x, y }),
         };
       });
-      setTree(updatedTree);
+      drawAxis(updatedTree);
     }
     updateTree();
 
@@ -59,88 +144,7 @@ export function TreeAxis(props: TreeAxisProps) {
         updateTree();
       },
     });
-  }, [sigma]);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    ctx.clearRect(0, 0, width!, height!);
-
-    const padding = 5;
-
-    // check the ratio of camera
-    const camera = sigma.getCamera();
-    const { ratio } = camera.getState();
-    const isSkip = Math.round(ratio);
-
-    const yAxis = chain(tree).groupBy("y").value();
-
-    const yAxisArray = sort(
-      Object.keys(yAxis).map((e) => Number(e)),
-      { order: "asc" }
-    );
-    let yAxisData = [];
-    if (isSkip >= 1) {
-      for (let i = 0; i < yAxisArray.length; ) {
-        yAxisData.push(yAxisArray[i]);
-        i += isSkip + 1;
-      }
-    } else {
-      yAxisData = yAxisArray;
-    }
-
-    // g value
-    const labelText: { [key: string]: [string, string] } = {};
-
-    forOwn(yAxis, (value, key) => {
-      const ave =
-        value.reduce((acc, curr) => acc + (curr?.g ?? 0), 0) / value.length;
-      const [coefficient, exp] = ave
-        .toExponential(2)
-        .split("e")
-        .map((item) => +item);
-      labelText[key] = [coefficient.toString(), exp.toString()];
-    });
-    const maxY = Math.max(...yAxisData);
-    const minY = Math.min(...yAxisData);
-
-    ctx.beginPath();
-    ctx.moveTo(5, minY);
-    ctx.lineTo(5, maxY);
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 1;
-    ctx.stroke();
-
-    ctx.font = "10px Arial";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Y Axis: G Value", padding, height * 0.98);
-
-    for (let i = 0; i <= yAxisData.length; i++) {
-      ctx.beginPath();
-      ctx.moveTo(padding, yAxisData[i]);
-      ctx.lineTo(padding + 5, yAxisData[i]);
-      ctx.stroke();
-
-      ctx.font = "10px Arial";
-      ctx.textAlign = "left";
-      ctx.textBaseline = "middle";
-      ctx.fillText(labelText[yAxisData[i]]?.[0], padding + 5, yAxisData[i]);
-
-      ctx.font = "8px Arial";
-      ctx.textAlign = "left";
-      ctx.textBaseline = "middle";
-      ctx.fillText(
-        labelText[yAxisData[i]]?.[1],
-        padding + 25,
-        yAxisData[i] - 4
-      );
-    }
-  }, [sigma, tree, width, height]);
+  }, [sigma, drawAxis, events]);
 
   return (
     <>
