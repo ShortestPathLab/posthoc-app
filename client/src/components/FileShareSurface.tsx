f9f50e6fa30a56d4f282c5fcdfd6bf0fca380fa0
Bennett <bennettmadavana@gmail.com>
2025-02-09 19:11:56 +1100

    Merge changes

---
+++ client/src/components/FileShareSurface.tsx
@@ -0,0 +1,121 @@
+import {
+  CloseOutlined,
+  LinkOutlined,
+  OpenInNewOutlined,
+} from "@mui-symbols-material/w400";
+import { Box, ListItemText, Stack, TextField, Typography } from "@mui/material";
+import copy from "clipboard-copy";
+import { useSm } from "hooks/useSmallDisplay";
+import { round } from "lodash";
+import { useMemo } from "react";
+import { FileMetadata } from "services/cloud-storage/CloudStorage";
+import { useCloudStorageService } from "slices/cloudStorage";
+import { usePaper } from "theme";
+import { useSnackbar } from "./generic/Snackbar";
+import { Button } from "./generic/inputs/Button";
+import { Surface } from "./generic/surface";
+
+export const FileShareSurface = ({ file }: { file: FileMetadata }) => {
+  const [{ instance: cloudService }] = useCloudStorageService();
+  const sm = useSm();
+  const paper = usePaper();
+  const notify = useSnackbar();
+  const link = useMemo(
+    () => cloudService?.getFileLink(file.id),
+    [cloudService, file.id]
+  );
+  const handleCopy = async () => {
+    try {
+      if (link) {
+        await copy(link);
+        notify("Copied to clipboard");
+        return link;
+      } else {
+        throw new Error("Unable to generate link");
+      }
+    } catch {
+      notify("Unable to generate link");
+    }
+  };
+  const handleShare = async () => {
+    try {
+      const link = cloudService?.getFileLink(file.id);
+      await navigator.share({
+        title: "Posthoc visualisation",
+        text: link ?? "",
+      });
+    } catch {
+      notify("Unable to generate link");
+    }
+  };
+  return (
+    <Stack sx={{ p: sm ? 2 : 3, gap: 2 }}>
+      <Box sx={{ width: 128, height: 128, ...paper(1) }} />
+      <ListItemText
+        primary={file.name}
+        secondary={
+          <>
+            <Typography variant="body2" color="textSecondary">
+              Type: {file.mimeType}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Size: {round(+file.size / 1024 / 1024, 2)} MB
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Modified: {new Date(file.modifiedTime).toLocaleString()}
+            </Typography>
+          </>
+        }
+      />
+      <Stack sx={{ gap: 1 }}>
+        <Surface
+          title="Shareable link"
+          trigger={({ open }) => {
+            return (
+              <Button
+                variant="contained"
+                startIcon={<LinkOutlined color="primary" />}
+                onClick={() => {
+                  if (link) {
+                    open();
+                  }
+                }}
+              >
+                Get shareable link
+              </Button>
+            );
+          }}
+        >
+          <Stack sx={{ p: sm ? 2 : 3, gap: 1, pb: 6 }} direction="row">
+            <TextField
+              sx={{ flex: 1 }}
+              label="Link"
+              variant="filled"
+              value={link}
+              autoFocus
+            />
+            <Button variant="outlined" onClick={handleCopy}>
+              Copy
+            </Button>
+          </Stack>
+        </Surface>
+        <Button
+          disabled={!navigator.share}
+          variant="text"
+          startIcon={<OpenInNewOutlined />}
+          onClick={handleShare}
+        >
+          Share via system
+        </Button>
+        <Button
+          disabled={!navigator.share}
+          variant="text"
+          startIcon={<CloseOutlined color="error" />}
+          onClick={handleShare}
+        >
+          Delete workspace
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
---
+++ client/src/services/cloud-storage/CloudStorageService.tsx
@@ -0,0 +1,39 @@
+import { has } from "lodash";
+import { useEffect } from "react";
+import { useAsync } from "react-async-hook";
+import { useLatest } from "react-use";
+import { useAuth } from "slices/auth";
+import { useCloudStorageService } from "slices/cloudStorage";
+import { defaultCloudStorage, useSettings } from "slices/settings";
+import { assert } from "utils/assert";
+import providers from "./providers";
+
+function useCloudStorageManager(provider: keyof typeof providers) {
+  //
+  // ─── Create Storage Provider ─────────────────────────────────────────
+
+  assert(has(providers, provider), "provider exists");
+  const [{ [provider]: s }, setAuthState, initialised] = useAuth();
+  const state = useLatest(s);
+  const { result: service } = useAsync(async () => {
+    if (!initialised) return;
+    const service = providers[provider].create(
+      async () => state.current,
+      async (s) => setAuthState(() => ({ [provider]: s }))
+    );
+    await service.authenticate();
+    return service;
+  }, [provider, state, initialised]);
+
+  // ─── Publicise Changes ───────────────────────────────────────────────
+
+  const [, setCloudStorageService] = useCloudStorageService();
+  useEffect(() => {
+    setCloudStorageService(() => ({ instance: service }));
+  }, [service]);
+}
+
+export function CloudStorageService() {
+  const [{ cloudStorageType = defaultCloudStorage }] = useSettings();
+  useCloudStorageManager(cloudStorageType);
+}
