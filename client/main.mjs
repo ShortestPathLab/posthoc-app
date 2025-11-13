0a0fe1b5530e76f77d6736312f77a71c9fcf6cb0
Kevin Zheng <kaiwen530@gmail.com>
2025-11-14 02:16:47 +1100

    Fix deployment issue

--- client/main.mjs
+++ client/main.mjs
@@ -1,8 +1,8 @@
-const electron = require("electron");
-const fs = require("fs");
-const http = require("http");
-const path = require("path");
-const url = require("url");
+import { app, BrowserWindow, ipcMain, shell } from "electron";
+import { existsSync, statSync, readFileSync } from "fs";
+import { createServer } from "http";
+import { join, resolve, parse } from "path";
+import { parse as _parse } from "url";
 
 const map = {
   ".css": "text/css",
@@ -15,41 +15,32 @@ const map = {
   ".svg": "image/svg+xml",
 };
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   // extract URL path
   let pathname1 = `./${
-    url.parse(
-      path.join(
-        electron.app.isPackaged
-          ? path.join(electron.app.getAppPath(), "dist")
-          : "dist",
-        req.url
-      )
+    _parse(
+      join(app.isPackaged ? join(app.getAppPath(), "dist") : "dist", req.url)
     ).pathname
   }`;
-  pathname1 = fs.existsSync(pathname1)
+  pathname1 = existsSync(pathname1)
     ? pathname1
-    : fs.existsSync(path.resolve(__dirname, pathname1))
-    ? path.resolve(__dirname, pathname1)
-    : `./${
-        url.parse(
-          path.join(
-            electron.app.isPackaged ? "resources/app/dist" : "dist",
-            req.url
-          )
-        ).pathname
-      }`;
-  let pathname = fs.existsSync(pathname1)
+    : existsSync(resolve(__dirname, pathname1))
+      ? resolve(__dirname, pathname1)
+      : `./${
+          _parse(join(app.isPackaged ? "resources/app/dist" : "dist", req.url))
+            .pathname
+        }`;
+  let pathname = existsSync(pathname1)
     ? pathname1
-    : path.resolve(__dirname, pathname1);
+    : resolve(__dirname, pathname1);
   // based on the URL path, extract the file extension. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
+  const ext = parse(pathname).ext;
 
-  if (fs.existsSync(pathname)) {
+  if (existsSync(pathname)) {
     // if is a directory search for index file matching the extension
-    if (fs.statSync(pathname).isDirectory()) pathname += "/index" + ext;
+    if (statSync(pathname).isDirectory()) pathname += "/index" + ext;
     try {
-      const data = fs.readFileSync(pathname);
+      const data = readFileSync(pathname);
       // if the file is found, set Content-type and send data
       res.setHeader("Content-type", map[ext] || "text/plain");
       res.end(data);
@@ -79,14 +70,14 @@ const a = {
   resizable: true,
   webPreferences: {
     zoomFactor: 0.9,
-    preload: path.resolve(__dirname, "preload.js"),
+    preload: resolve(__dirname, "preload.js"),
   },
 };
 server.listen(0, () => {
   const port = server.address().port;
   console.log("Listening on port:", port);
   const createWindow = () => {
-    const win = new electron.BrowserWindow({ ...a, show: false });
+    const win = new BrowserWindow({ ...a, show: false });
     win.webContents.setWindowOpenHandler(makeWindowOpenHandler(port));
     win.loadURL(`http://localhost:${port}/index.html`);
     win.maximize();
@@ -94,8 +85,8 @@ server.listen(0, () => {
     win.webContents.on("did-finish-load", () => {
       win.webContents.setZoomFactor(0.9);
     });
-    electron.ipcMain.handle("title-bar", (_e, background, foreground) => {
-      const win2 = electron.BrowserWindow.getAllWindows().find(
+    ipcMain.handle("title-bar", (_e, background, foreground) => {
+      const win2 = BrowserWindow.getAllWindows().find(
         (c) => c.webContents.id === _e.sender.id
       );
       if (win2 && "setTitleBarOverlay" in win2) {
@@ -106,14 +97,14 @@ server.listen(0, () => {
       }
     });
   };
-  electron.app.whenReady().then(() => {
+  app.whenReady().then(() => {
     createWindow();
   });
-  electron.app.on("browser-window-created", (e, w) => {
+  app.on("browser-window-created", (e, w) => {
     w.webContents.setWindowOpenHandler(makeWindowOpenHandler(port));
   });
-  electron.app.on("window-all-closed", () => {
-    electron.app.quit();
+  app.on("window-all-closed", () => {
+    app.quit();
   });
 });
 
@@ -127,7 +118,7 @@ function makeWindowOpenHandler(port) {
         outlivesOpener: true,
       };
     } else {
-      electron.shell.openExternal(url);
+      shell.openExternal(url);
       return { action: "deny" };
     }
   };
