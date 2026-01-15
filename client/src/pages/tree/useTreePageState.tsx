583d70303dd71f62770c5866ccf7931232eee100
Kevin Zheng <kaiwen530@gmail.com>
2026-01-16 00:34:34 +1100

    refactor: migrate `useTreePageState` to use `useOne` and `id` for state selection.

--- client/src/pages/tree/useTreePageState.tsx
+++ client/src/pages/tree/useTreePageState.tsx
@@ -1,16 +1,11 @@
 import { slice } from "slices";
-import { equal } from "slices/selector";
-import { UploadedTrace } from "slices/UIState";
+import { id } from "slices/selector";
+import { useOne } from "slices/useOne";
 import { TreeLayer } from "./TreeLayer";
 
 export function useTreePageState(key?: string) {
-  "use no memo";
-
   const one = slice.layers.one<TreeLayer>(key);
-  const trace = one.use<UploadedTrace | undefined>(
-    (l) => l?.source?.trace,
-    equal("key"),
-  );
-  const step = one.use((l) => l?.source?.step);
+  const trace = useOne(one, (l) => l?.source?.trace, id("key"));
+  const step = useOne(one, (l) => l?.source?.step);
   return { step, trace };
 }
